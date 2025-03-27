8f620c673a02663ac6daf842029993bf7d9d5d47
zain azhar <142168421+ZAINAZHAR303@users.noreply.github.com>
2025-03-27 22:03:45 +0500

    add the user button in the home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,17 @@
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
+import { currentUser } from '@clerk/nextjs/server'
 
-export default function Home() {
+export default async function Home() {
+
+  const user = await currentUser()
+  if (!user) return <div>Not signed in</div>
   return (
     <div >
      
       Home
       <UserButton afterSignOutUrl="/sign-in" />
+      <div>Hello {user?.firstName}</div>
     </div>
   );
 }
